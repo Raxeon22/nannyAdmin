a6b16bceeac78e3c883295e75c6dca46290b3b71
huzaifa2213 <huzaifa.kamran1322@gmail.com>
2022-06-16 17:22:48 +0500

    api url changed

--- src/middleware/API.js
+++ src/middleware/API.js
@@ -5,5 +5,5 @@ export default axios.create({
     // baseURL: `http://localhost:4000`
     // baseURL: `http://3.94.191.228:4000`
     // baseURL: `https://nanyapp2.herokuapp.com`
-    baseURL: `https://nanybackendforadmin.herokuapp.com`
+    baseURL: `https://nanny-backend-app.herokuapp.com`
 })
